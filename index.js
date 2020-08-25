6048586aa631d7de32db2379406da4893417c9e4
SadGuy <57141652+SadGuy1@users.noreply.github.com>
2020-08-25 10:39:06 +0200

    index.js v1.0
    Fixed Bugs.

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-// Template by SadGuy
 const { Client, MessageEmbed } = require('discord.js');
 const Discord = require('discord.js');
-const TOKEN = 'YOUR_BOT_TOKEN'
+const TOKEN = 'your_bot_token'
 const client = new Client();
    client.on('ready', () => {
         client.user.setActivity('YOUR_BOT_STATUS ', { type: 'PLAYING' });
         console.log(`${client.user.username} logged in!`);
     })
-
 client.on('message', message => {
   if (message.content === '!embed') {
 const exampleEmbed = new Discord.MessageEmbed()
 	.setColor('#0099ff')
 	.setTitle('your_embed_title')
 	.setDescription('your_embed_description')
-	.setThumbnail('https://cdn.sadguy.pl/admin.png'')
+	.setThumbnail('https://cdn.sadguy.pl/admin.png')
 	.addFields(
 		{ name: '\u200B', value: 'embed_field_1' },
 	)
@@ -25,3 +23,5 @@ message.channel.send(exampleEmbed);
 }});
 // If there are bugs here, comment and I'll try to fix them as soon as possible.
 // Check out my website! sadguy.pl
+
+client.login(TOKEN);
